15089d89cc4b6cc61216c6a4eafcd1024652341b
fouche <tetrapharmakon@gmail.com>
2019-05-05 16:35:25 +0200

    fetch arXiv papers' titles

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import           Lib
+import           Text.XML.HXT.Core
+import           Text.XML.HXT.DOM.FormatXmlTree
+import           Text.XML.HXT.HTTP
+
+path :: String
+path = "http://export.arxiv.org/api/query?search_query=all:coend&start=0&max_results=10"
+
+selectAllEntries :: ArrowXml a => a XmlTree XmlTree
+selectAllEntries = deep (isElem >>> hasName "entry")
+
+selectAllTitle :: ArrowXml a => a XmlTree XmlTree
+selectAllTitle = deep (isElem >>> hasName "title")
 
 main :: IO ()
-main = someFunc
+main = do
+  x <- runX (readDocument [withHTTP []] path
+    >>> selectAllEntries
+    >>> selectAllTitle
+    >>> deep isText
+    >>> getText)
+  putStrLn $ unlines x
