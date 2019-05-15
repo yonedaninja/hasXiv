527abd53842145073121de920c491f6869115bb2
fouche <tetrapharmakon@gmail.com>
2019-05-15 12:39:42 +0200

    improve readability

--- app/Main.hs
+++ app/Main.hs
@@ -7,10 +7,12 @@ import           Text.XML.HXT.Core
 import           Text.XML.HXT.DOM.FormatXmlTree
 import           Text.XML.HXT.HTTP
 
-import Control.Concurrent.Async
+import           Control.Concurrent.Async
 
 paths :: [String]
-paths = ["http://export.arxiv.org/api/query?search_query=all:the&start=" ++ (show $ i*2000) ++ "&max_results=2000" | i <- [0..14]]
+paths = [  "http://export.arxiv.org/api/query?search_query=all:the&start="
+        ++ show (i * 2000)
+        ++ "&max_results=2000" | i <- [0..14]]
 
 selectAllEntries :: ArrowXml a => a XmlTree XmlTree
 selectAllEntries = deep (isElem >>> hasName "entry")
@@ -28,5 +30,5 @@ getDaTitles path = runX (readDocument [withHTTP []] path
 main :: IO ()
 main = do
   xs <- mapConcurrently getDaTitles paths
-  let zs = map (\x -> filter (/='\n') x) $ concat xs
+  let zs = map (filter (/= '\n')) $ concat xs
   putStrLn $ unlines zs
