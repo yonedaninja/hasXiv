7014fc8b568a0256285b76b194becf83a590bbb7
fouche <tetrapharmakon@gmail.com>
2020-02-19 15:49:46 +0100

    catchup commit

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,19 @@
 
 module Main where
 
-import           Lib
-import           Text.XML.HXT.Core
-import           Text.XML.HXT.DOM.FormatXmlTree
-import           Text.XML.HXT.HTTP
+import Library
+import Text.XML.HXT.Core
+import Text.XML.HXT.DOM.FormatXmlTree
+import Text.XML.HXT.HTTP
 
-import           Control.Concurrent.Async
+import Control.Concurrent.Async
 
-paths :: [String]
-paths = [  "http://export.arxiv.org/api/query?search_query=all:the&start="
-        ++ show (i * 2000)
-        ++ "&max_results=2000" | i <- [0..14]]
+paths :: String -> [String]
+paths qry =
+  [ "http://export.arxiv.org/api/query?search_query=all:" ++
+  qry ++ "&start=" ++ show (i * 5) ++ "&max_results=5"
+  | i <- [0 .. 14]
+  ]
 
 selectAllEntries :: ArrowXml a => a XmlTree XmlTree
 selectAllEntries = deep (isElem >>> hasName "entry")
@@ -20,15 +22,15 @@ selectAllEntries = deep (isElem >>> hasName "entry")
 selectAllTitle :: ArrowXml a => a XmlTree XmlTree
 selectAllTitle = deep (isElem >>> hasName "title")
 
-getDaTitles path = runX (readDocument [withHTTP []] path
-    >>> selectAllEntries
-    >>> selectAllTitle
-    >>> deep isText
-    >>> getText)
-
+getDaTitles :: String -> IO [String]
+getDaTitles path =
+  runX
+    (readDocument [withHTTP []] path >>>
+     selectAllEntries >>> selectAllTitle >>> deep isText >>> getText)
 
 main :: IO ()
 main = do
-  xs <- mapConcurrently getDaTitles paths
+  query <- getLine
+  xs <- mapConcurrently getDaTitles (paths query)
   let zs = map (filter (/= '\n')) $ concat xs
   putStrLn $ unlines zs
--- src/Library.hs
+++ src/Library.hs
@@ -1,6 +1,6 @@
-module Lib
-    ( someFunc
-    ) where
+module Library
+  ( someFunc
+  ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
